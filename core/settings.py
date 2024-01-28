171c69ddd64b75b9d469880fab17cb2e7e9e3afa
hudeh <hudeh30@gmail.com>
2024-01-28 19:46:03 +0100

    settings

--- core/settings.py
+++ core/settings.py
@@ -2,6 +2,10 @@ import datetime
 import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
+from sentry_sdk.integrations.django import DjangoIntegration
+import sentry_sdk
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,10 +14,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 ADMINS = (("Henry Udeh", "hudeh30@gmail.com"),)
 MANAGERS = ADMINS
 SECRET_KEY = config("SECRET_KEY")
-ALLOWED_HOSTS = ["*"]
+DEBUG = config("DEBUG", default=False, cast=bool)
 
+ALLOWED_HOSTS = ["nsc-test-4db42f397f71.herokuapp.com"]
 
-ALLOWED_HOSTS = []
 
 LOGOUT_REDIRECT_URL = "/"
 LOGIN_REDIRECT_URL = "/rooms/"
@@ -52,7 +56,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "core.urls"
-
+WSGI_APPLICATION = "core.wsgi.application"
+ASGI_APPLICATION = "core.asgi.application"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -68,8 +73,75 @@ TEMPLATES = [
         },
     },
 ]
+# database
+DATABASES = {
+    "default": dj_database_url.config(
+        "DATABASE_URL", conn_max_age=600, ssl_require=True
+    )
+}
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
 
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:3002",
+    "http://localhost:3003",
+    "https://support.hotelproxi.com",
+    "https://nsc-test-4db42f397f71.herokuapp.com",
+)
+CORS_ORIGIN_REGEX_WHITELIST = [
+    r"^https://\w+\.hotelproxi.com$",
+    r"^https://\w+\.herokuapp.com$",
+    r"^http://\w+\:3000$",
+    r"^http://\w+\:3001$",
+    r"^http://\w+\:3002$",
+]
+CORS_ORIGIN_ALLOW_ALL = False
+# enforce https
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+ASGI_APPLICATION = "core.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [config("REDIS_URL")],
+        },
+    },
+}
 
+sentry_sdk.init(
+    dsn=config("SENTRY_DSN"),
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+    send_default_pii=True,
+)
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID", default="")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY", default="")
+AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME", default="")
+AWS_DEFAULT_ACL = None
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+# s3 static settings
+STATIC_LOCATION = "static"
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
+STATICFILES_STORAGE = "core.storage_backends.StaticStorage"
+# s3 public media settings
+PUBLIC_MEDIA_LOCATION = "media"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
+DEFAULT_FILE_STORAGE = "core.storage_backends.PublicMediaStorage"
+# s3 private media settings
+PRIVATE_MEDIA_LOCATION = "private"
+PRIVATE_FILE_STORAGE = "core.storage_backends.PrivateMediaStorage"
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -112,38 +184,4 @@ REDOC_SETTINGS = {
     "LAZY_RENDERING": False,
 }
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-STATIC_URL = "/staticfiles/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-# Define MEDIA_URL to determine the base URL for serving media files
-MEDIA_URL = "/media/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-
-#  CORS
-
-
-CORS_ALLOW_ALL_ORIGINS = True
-
-# Load the default ones
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "http://localhost:3002",
-]
-CORS_ORIGIN_WHITELIST = (
-    "http://localhost:3000",
-    "http://*.localhost:3000",
-    "http://*.localhost:3001",
-    "http://localhost:3001",
-    "http://localhost:3002",
-)
-CORS_ORIGIN_WHITELIST = (
-    "http://localhost:3000",
-    "http://*.localhost:3000",
-    "http://localhost:3001",
-    "http://localhost:3002",
-)
-CSRF_TRUSTED_ORIGINS = ["http://*.localhost", "http://localhost:3000"]
 AUTH_USER_MODEL = "user.ChatUser"
