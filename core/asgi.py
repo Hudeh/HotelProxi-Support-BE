da8f8373e063b7ded08a6c5f35627e6a3beff055
hudeh <hudeh30@gmail.com>
2024-01-28 18:54:59 +0100

    chan

--- core/asgi.py
+++ core/asgi.py
@@ -1,13 +1,11 @@
 import os
-
 from django.core.asgi import get_asgi_application
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-
+from decouple import config
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", config("ENVIRONMENT"))
 
 application = ProtocolTypeRouter(
     {
