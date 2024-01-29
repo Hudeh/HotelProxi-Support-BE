7d880205c7832c101e8b5cd8f632911c7e986d47
hudeh <hudeh30@gmail.com>
2024-01-29 09:45:01 +0100

    host

--- core/storage_backends.py
+++ core/storage_backends.py
@@ -1,35 +1,20 @@
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
-from django_tenants.utils import parse_tenant_config_path
 
 
 class StaticStorage(S3Boto3Storage):
-    location = "static"
-    default_acl = "public-read"
-
-    @property
-    def location(self) -> str:
-        _location = parse_tenant_config_path("%s")
-        return _location
+    location = 'static'
+    default_acl = 'public-read'
 
 
 class PublicMediaStorage(S3Boto3Storage):
-    location = "media"
-    default_acl = "public-read"
+    location = 'media'
+    default_acl = 'public-read'
     file_overwrite = False
-
-    @property
-    def location(self) -> str:
-        _location = parse_tenant_config_path("%s")
-        return _location
 
 
 class PrivateMediaStorage(S3Boto3Storage):
-    location = "private"
-    default_acl = "private"
+    location = 'private'
+    default_acl = 'private'
     file_overwrite = False
     custom_domain = False
-
-    @property
-    def location(self) -> str:
-        _location = parse_tenant_config_path("%s")
-        return _location
