3c38f2812f67a45748eaeebdec92628557c0f05e
hudeh <hudeh30@gmail.com>
2024-01-29 09:10:36 +0100

    redis

--- core/asgi.py
+++ core/asgi.py
@@ -1,17 +1,26 @@
+# import os
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
+# django_asgi_app = get_asgi_application()
+# from chat.routing import websocket_urlpatterns
+
+
+# application = ProtocolTypeRouter(
+#     {
+#         "http": django_asgi_app,
+#         "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+#     }
+# )
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-django_asgi_app = get_asgi_application()
-from chat.routing import websocket_urlpatterns
+django.setup()
 
-
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
-    }
-)
+application = get_default_application()
