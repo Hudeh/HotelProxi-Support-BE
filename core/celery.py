7c3538617d8be28ef7d823486563047dff509dd3
hudeh <hudeh30@gmail.com>
2024-01-29 06:04:16 +0100

    redis

--- core/celery.py
+++ core/celery.py
@@ -12,10 +12,7 @@ app = Celery(
     "core",
     broker=config("REDIS_URL"),
     backend=config("REDIS_URL"),
-    redbeat_redis_url=config("REDIS_URL"),
-    broker_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE},
-    redis_backend_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE},
-    broker_connection_retry_on_startup=True
+    redbeat_redis_url=config("REDIS_URL")
 )
 
 app.config_from_object(settings, namespace="CELERY")
